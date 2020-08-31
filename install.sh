53ad460bf41bc4ad7d68d7a3ee1bab4d08310193
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-08-31 12:33:52 +0200

    Remove install script

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x -e
-
-cp -fv captive-portal.py /usr/local/sbin/captive-portal
-cp -fv captive-portal@.service /lib/systemd/system/
-chmod 644 /lib/systemd/system/captive-portal@.service
-pipenv install --system --deploy
